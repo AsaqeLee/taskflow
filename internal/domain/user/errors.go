a288971287f90f3a6bbfbb5fad36cba6d94a46c8
AsaqeLee <tianleili2001@gmail.com>
2026-06-17 15:39:45 +0800

    Format Go files for CI

--- internal/domain/user/errors.go
+++ internal/domain/user/errors.go
@@ -3,11 +3,11 @@ package user
 import "errors"
 
 var (
-	ErrEmptyUserID      = errors.New("user id is required")
-	ErrEmptyUserName    = errors.New("user name is required")
-	ErrInvalidRole      = errors.New("user role is invalid")
-	ErrAccountDisabled  = errors.New("account is disabled")
-	ErrForbiddenDisable = errors.New("current user cannot disable this account")
-	ErrAlreadyDisabled   = errors.New("account is already disabled")
+	ErrEmptyUserID         = errors.New("user id is required")
+	ErrEmptyUserName       = errors.New("user name is required")
+	ErrInvalidRole         = errors.New("user role is invalid")
+	ErrAccountDisabled     = errors.New("account is disabled")
+	ErrForbiddenDisable    = errors.New("current user cannot disable this account")
+	ErrAlreadyDisabled     = errors.New("account is already disabled")
 	ErrForbiddenUserCreate = errors.New("only owners can create users")
 )
